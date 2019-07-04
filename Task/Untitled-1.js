f7ce0e53643ffe16272888dde92b67cd4fe9a6a5
caktuts <48790595+caktuts@users.noreply.github.com>
2019-07-04 09:23:51 +0300

    Add files via upload

---
+++ Task/Untitled-1.js
@@ -0,0 +1,9 @@
+let a = prompt("Введите число :", '');
+let b = prompt("Введите число :", '');
+let c = prompt("Введите число :", '');
+let d = prompt("Введите число :", '');
+let f = prompt("Введите число :", '');
+let g = prompt("Введите число :", '');
+let y = [a,b,c,d,f,g];
+let z = 15;
+alert(y + " " + ' ' + z);
